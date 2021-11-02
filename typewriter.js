304eaab21a08cd1225dc4dc48118ad0ecbc84000
spatel93 <sid.auburn@gmail.com>
2021-11-02 20:48:38 +0000

    implemented newline character.

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,26 @@
 const sentence = "hello there from lighthouse labs";
 let timer = 0
 
-for(const c of sentence){
-  timer += 500
-  setTimeout(()=>{
-    process.stdout.write(c)
-  }, timer)
+// for(const c of sentence){
+//   timer += 500
+//   setTimeout(()=>{
+//     process.stdout.write(c)
+//   }, timer)
+  
+// }
+
+
+for(const i in sentence){
+  if(i == (sentence.length-1)){
+    timer += 100
+    setTimeout(()=>{
+      process.stdout.write(sentence[i]+'\n')
+    }, timer)
+  }
+  else{
+    timer += 100
+    setTimeout(()=>{
+      process.stdout.write(sentence[i])
+    }, timer)
+  }
 }
-
-
